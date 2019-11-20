f51da7780f3118f0977e9a7074b0e3d1af2c6839
Radek M <crushyna@gmail.com>
2019-11-20 16:37:53 +0100

    major fixed added initial version of filespec extractor

---
+++ data/filespec_extractor.py
@@ -0,0 +1,61 @@
+import xml.etree.ElementTree as ET
+from os import listdir
+
+
+class FileSpecsFinder:
+    """
+    testing purposes only
+    not a working version
+    """
+
+    @staticmethod
+    def get_list_of_filespec_files():
+        filespecs_file_list = listdir(r'data\filespecs')
+        return filespecs_file_list
+
+    @staticmethod
+    def get_dict_of_templates():
+        templates_list = []
+        for each_filespec_object in filespecs_objects:
+            templates_list.append(each_filespec_object.get_filespec_name())
+
+        return templates_list
+
+
+
+class FileSpecParser:
+    """
+    testing purposes only
+    not a working version
+    """
+
+    def __init__(self, xml_filename: str):
+        self.xml_filename = xml_filename
+        self.filespec_name = str(xml_filename[9:-4])
+        self.fieldnames_list = []
+
+    def get_filespec_name(self):
+        return self.filespec_name
+
+    def get_list_of_fieldnames(self):
+        tree = ET.parse(self.xml_filename)
+        root = tree.getroot()
+
+        for each_rfrecordspec in root.iter('rfrecordspec'):
+            # print(each_rfrecordspec.attrib['fieldname'])
+            self.fieldnames_list.append(each_rfrecordspec.attrib['fieldname'])
+
+
+# testing commands
+
+# parsedXML_1.get_list_of_fieldnames()
+
+filespecs_list = FileSpecsFinder.get_list_of_filespec_files()
+print(filespecs_list)
+
+filespecs_objects = []
+for each_filespec_file in filespecs_list:
+    filespecs_objects.append(FileSpecParser(xml_filename=each_filespec_file))
+
+print(filespecs_objects)
+
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import sys
-import pandas as pd
 from os import listdir
-import data.functions
-import data.templateColumns
-from data.functions import createExcelFile
-from data.functions import createTextFromExcelFile
+from data.convert_functions import ConvertFunctions
+from data.filespec_extractor import *
 import time
 import os
 import openpyxl
@@ -12,7 +9,14 @@ import xlrd
 
 PROGRAM_VERSION = '0.3'
 
-templates = {1:'CUSTOMER_POLICY_LINK', 2:'CUSTOMERS', 3:'INTERMEDIARIES', 4:'INTERMEDIARY_POLICY_LINK', 5:'INTERMEDIARY_TYPE', 6:'OPERATIONS', 7:'PARTY_ROLE', 8:'POLICIES', 9:'POLICY_STATUS', 10:'PRODUCT_SOURCE_TYPE', 11:'COUNTRY', 12:'PRODUCT', 13:'TRANSACTIONS'}
+'''
+templates = {1: 'CUSTOMER_POLICY_LINK', 2: 'CUSTOMERS', 3: 'INTERMEDIARIES', 4: 'INTERMEDIARY_POLICY_LINK',
+             5: 'INTERMEDIARY_TYPE', 6: 'OPERATIONS', 7: 'PARTY_ROLE', 8: 'POLICIES', 9: 'POLICY_STATUS',
+             10: 'PRODUCT_SOURCE_TYPE', 11: 'COUNTRY', 12: 'PRODUCT', 13: 'TRANSACTIONS'}
+'''
+
+templates = FileSpecsFinder.get_dict_of_filespec_files()
+
 
 currentFile = 'none'
 fileTemplate = 'none'
@@ -27,70 +31,69 @@ while main_menu:
     3.Generate fixed TXT file
     4.Exit/Quit
     """)
-    ans=input("What would you like to do? ")
+    ans = input("What would you like to do? ")
 
-    if ans=="1":
-      print("\n Load TXT file:")
-      menu_1 = True
+    if ans == "1":
+        print("\n Load TXT file:")
+        menu_1 = True
 
-      txtFilesList = listdir('input')
-      txtFilesNumber = len(txtFilesList)
-      txtFilesDict = dict(zip(txtFilesList, range(txtFilesNumber)))
-      txtFilesDictFixed = {v: k for k, v in txtFilesDict.items()}
-      for key, value in txtFilesDictFixed.items():
-        print(key, ": ", value)
-      
-      selectedFileId = int(input("Enter requested file ID: "))
-      if selectedFileId < txtFilesNumber:
-          currentFile = txtFilesDictFixed.get(selectedFileId)
-          print("File loaded!\n")
-          time.sleep(1)
-          print(templates)
-          selectedTemplateKey = int(input("Select template: "))
-          if int(selectedTemplateKey) <= len(templates):
-              print("Selected template: ")
-              print(templates.get(selectedTemplateKey))
-              time.sleep(1)
-              fileTemplate = templates.get(selectedTemplateKey)
-          else:
-              print("Inproper selection, try again!")
-              time.sleep(2)
-      else:
-          print("Inproper file name. Try again!")
-          time.sleep(2) 
-      
-    elif ans=="2":
-      print("\n Generate XLSX file: ")
-      menu_2 = True
-      print("Selected filename: " + currentFile)
-      print("Selected template: " + fileTemplate)
-      time.sleep(1)
-      createExcelFile(currentFile, fileTemplate)
+        txtFilesList = listdir('input')
+        txtFilesNumber = len(txtFilesList)
+        txtFilesDict = dict(zip(txtFilesList, range(txtFilesNumber)))
+        txtFilesDictFixed = {v: k for k, v in txtFilesDict.items()}
+        for key, value in txtFilesDictFixed.items():
+            print(key, ": ", value)
 
-    elif ans=="3":
-      print("\n Generate fixed TXT file:")
-      menu_3 = True
+        selectedFileId = int(input("Enter requested file ID: "))
+        if selectedFileId < txtFilesNumber:
+            currentFile = txtFilesDictFixed.get(selectedFileId)
+            print("File loaded!\n")
+            time.sleep(1)
+            print(templates)
+            selectedTemplateKey = int(input("Select template: "))
+            if int(selectedTemplateKey) <= len(templates):
+                print("Selected template: ")
+                print(templates.get(selectedTemplateKey))
+                time.sleep(1)
+                fileTemplate = templates.get(selectedTemplateKey)
+            else:
+                print("Improper selection, try again!")
+                time.sleep(2)
+        else:
+            print("Improper file name. Try again!")
+            time.sleep(2)
 
-      excelFilesList = listdir('output_xlsx')
-      excelFilesNumber = len(excelFilesList)
-      excelFilesDict = dict(zip(excelFilesList, range(excelFilesNumber)))
-      excelFilesDictFixed = {v: k for k, v in excelFilesDict.items()}
-      for key, value in excelFilesDictFixed.items():
-        print(key, ": ", value)
-      
-      selectedFileId = int(input("Enter requested file ID: "))
-      if selectedFileId < excelFilesNumber:
-          currentExcelFile = excelFilesDictFixed.get(selectedFileId)
-          createTextFromExcelFile(currentExcelFile)
-      else:
-        print("Inproper file ID. Try again!")
+    elif ans == "2":
+        print("\n Generate XLSX file: ")
+        menu_2 = True
+        print("Selected filename: " + currentFile)
+        print("Selected template: " + fileTemplate)
+        time.sleep(1)
+        ConvertFunctions.createExcelFile(currentFile, fileTemplate)
+
+    elif ans == "3":
+        print("\n Generate fixed TXT file:")
+        menu_3 = True
+        excelFilesList = listdir('output_xlsx')
+        excelFilesNumber = len(excelFilesList)
+        excelFilesDict = dict(zip(excelFilesList, range(excelFilesNumber)))
+        excelFilesDictFixed = {v: k for k, v in excelFilesDict.items()}
+        for key, value in excelFilesDictFixed.items():
+            print(key, ": ", value)
+
+        selectedFileId = int(input("Enter requested file ID: "))
+        if selectedFileId < excelFilesNumber:
+            currentExcelFile = excelFilesDictFixed.get(selectedFileId)
+            ConvertFunctions.createTextFromExcelFile(currentExcelFile)
+        else:
+            print("Inproper file ID. Try again!")
+            time.sleep(2)
+
+    elif ans == "4":
+        print("\n Goodbye!")
+        main_menu = None
         time.sleep(2)
 
-    elif ans=="4":
-      print("\n Goodbye!")
-      main_menu = None
-      time.sleep(2)
-
     else:
-      print("\n Not valid choice, try again!")
-      time.sleep(2)
+        print("\n Not valid choice, try again!")
+        time.sleep(2)
