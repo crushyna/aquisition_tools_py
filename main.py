b047fff5cb063724ea906dba90c9822b0e4cf0e8
Radek M <crushyna@gmail.com>
2019-11-29 14:28:00 +0100

    minor fixes

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import time
 import openpyxl
 import xlrd
 
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 PROGRAM_VERSION = '0.6'
 
 host = '10.227.7.120'
